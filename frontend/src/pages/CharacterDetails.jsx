5be8b35ad82cf6120977bf892bfef0f9ae9ecb87
SleepyDroids <jacquelinelwrites@gmail.com>
2025-11-18 09:43:02 -0500

    used conditional rendering to deal with stats loading as undefined

--- frontend/src/pages/CharacterDetails.jsx
+++ frontend/src/pages/CharacterDetails.jsx
@@ -16,8 +16,7 @@ export default function CharacterDetails({ data }) {
     }
   }, [details]);
 
-  // const { str, int, wis, cha, con, dex } = details.stats
-  // console.log(details.stats)
+  // console.log(details.stats);
 
   return (
     <div className="character-details">
@@ -42,42 +41,65 @@ export default function CharacterDetails({ data }) {
           <span className="details-text">Level:</span> {details.level}
         </p>
 
-<table className="stats-table">
-    {/* <caption>{details.name} Stat Table</caption> */}
-    <thead>
-        <tr>
-            <th>Stat</th>
-            <th>Value</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Strength</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>Dexterity</td>
-            <td>12</td>
-        </tr>
-        <tr>
-            <td>Constitution</td>
-            <td>3</td>
-        </tr>
-        <tr>
-            <td>Intelligence</td>
-            <td>4</td>
-        </tr>
-        <tr>
-            <td>Wisdom</td>
-            <td>5</td>
-        </tr>
-        <tr>
-            <td>Charisma</td>
-            <td>1</td>
-        </tr>
-    </tbody>
-</table>
-
+        <table className="stats-table">
+          {/* <caption>{details.name} Stat Table</caption> */}
+          <thead>
+            <tr>
+              <th>Stat</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Strength</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.str}</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Dexterity</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.dex}</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Constitution</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.con}</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Intelligence</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.int}</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Wisdom</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.wis}</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Charisma</td>
+              <td>
+                {details.stats && (
+                  <span className="stat-num">{details.stats.cha}</span>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
       <div className="character-notes">
