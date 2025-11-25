9c5a2af5f5a5ac7ddbcd8ce5431b29a5e95fb404
SleepyDroids <jacquelinelwrites@gmail.com>
2025-11-24 20:00:00 -0500

    slightly restructured loading component and added styling to volo

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,13 +1,22 @@
-import volo from "../assets/volo-loading.webp"
+import volo from "../assets/volo-loading.webp";
 
-export default function Loading(){
-    return(
-        <div className="loading">
-            <img src={volo} alt="Image of Volo from Volo's Guide to Monsters" /> 
-        <p>Rolls in progress...</p>
-            <div className="loading-msg">
-                <p>Hold tight—Volo is dusting off your character sheets for display.</p>
-            </div>
+export default function Loading() {
+  return (
+    <div className="loading">
+      <img
+        src={volo}
+        alt="Image of Volo from Volo's Guide to Monsters"
+        className="loading-image"
+      />
+
+      <div className="loading-text">
+        <h1>Rolls in progress...</h1>
+        <div className="loading-msg">
+          <p>
+            Hold tight—Volo is dusting off your character sheets for display.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
