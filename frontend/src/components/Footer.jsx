3738339699e5778c1bc7549bca00d7c32897504b
SleepyDroids <jacquelinelwrites@gmail.com>
2025-11-24 20:30:56 -0500

    stopped my footer from blowing up my whole app but now need to fix character card page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,20 @@
 export default function Footer() {
-
   return (
-    <footer id="footer">
-      <p className="footer-tagline">A project by <a className="footer-link" href="https://github.com/SleepyDroids" target="_blank">SleepyDroids</a>.</p>
-
-<p className="footer-tagline footer-creds">
-Dungeons &amp; Dragons and all related marks are &copy; Wizards of the Coast.
+    <footer id="footer" className="footer">
+      <p>
+        {/* Dungeons &amp; Dragons and all related marks are &copy; Wizards of the Coast.
         Pathfinder is &copy; Paizo Inc. Character Cubby is a personal, non-commercial
-        fan project for organizing your adventuring parties.
-</p>
-
+        fan project for organizing your adventuring parties. */}
+        A project by{" "}
+        <a
+          className="footer-link"
+          href="https://github.com/SleepyDroids"
+          target="_blank"
+        >
+          SleepyDroids
+        </a>
+        .
+      </p>
     </footer>
-  )
+  );
 }
